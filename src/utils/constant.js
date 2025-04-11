b4acaef412e29063f173ff2e160e20d3225debbf
UrvashiLathiya70 <urvashil9723@gmail.com>
2025-04-11 15:53:52 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,6 @@
 
-export const BASE_URL = 'https://crowdfundingapi.project-demo.info:3002';
-// export const BASE_URL = "http://192.168.0.29:3002";
 
-// export const BASE_URL = '${BASE_URL}';
+export const BASE_URL = "http://192.168.0.29:3002";
 
 export const ROLESLIST = [
     { id: 1, title: "Admin", value: "Admin" },
